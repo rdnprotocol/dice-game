0805a1f72fac7c1b110dbb9cb897d24aece55901
tuulAtHome <rdnprotocol@gmail.com>
2020-11-16 13:01:02 +0800

    toglogch huwaari solih

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Тоглогчийн ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогчийг 0, хоёрдугаар тоглогчийг 1 гэж үзлээ
-var activePlayer = 1;
+var activePlayer = 0;
 
 // Тоглогчдийн цуглуулсан оноог хадгалах хувьсагч
 
@@ -20,8 +20,20 @@ document.getElementById("current-0").textContent = "0";
 document.getElementById("current-1").textContent = "0";
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
+//Шоог шидэх эвент листенер
 document.querySelector(".btn-roll").addEventListener("click", function () {
+  // 1-6 доторх тооны санамсаргүй тоог гаргаж авах
   var diceNumber = Math.floor(Math.random() * 6) + 1;
+  // шооны зургийг вэб дээр гаргаж ирнэ
   diceDom.style.display = "block";
+  // буусан тооны харгалзах шооны зураг гаргаж ирэх
   diceDom.src = "dice-" + diceNumber + ".png";
+  // буусан тоо нь 1 ээс ялгаатай бол идвэхтэй тоглогчийн Шооны тоглогчийн ээлжийн оноог нэмэгдүүлэх
+  if (diceNumber !== 1) {
+    //1 ээс ялгаатай тоо буулаа буусан тоог тоглогчид нэмж өгнө
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    //1 буусан бол
+  }
 });
